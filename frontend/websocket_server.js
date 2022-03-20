abe1a3074aafd36bf1ca4de24e9642a89056cfb9
tlewis11 <tlewis11@github.com>
2022-03-20 13:36:25 -0500

    remove unused socketserver file

--- frontend/websocket_server.js
+++
@@ -1,9 +0,0 @@
-const io = require('socket.io')();
-const port = 8001;
-io.on('connection', (socket) => {
-    socket.on('chat message',(msg) => {
-        console.log("messae: " + msg)
-    });
-})
-io.listen(port);
-console.log('Listening on port ' + port + '...');
